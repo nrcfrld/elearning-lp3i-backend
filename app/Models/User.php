eb0aecb4babc04ad5c034ab645ea597a573fea2e
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2021-11-02 12:19:13 +0700

    Update Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    protected $guard_name = 'api';
+
     /**
      * The attributes that are mass assignable.
      *
