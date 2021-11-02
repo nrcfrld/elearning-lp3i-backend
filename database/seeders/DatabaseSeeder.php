bae4c571f2ebd12d67fc53bc30dbeaa958599167
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2021-11-02 12:05:51 +0700

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            RoleSeeder::class,
+            RolePermissionSeeder::class,
             ConfigurationSeeder::class,
             UserSeeder::class,
         ]);
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
-class RoleSeeder extends Seeder
+class RolePermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
