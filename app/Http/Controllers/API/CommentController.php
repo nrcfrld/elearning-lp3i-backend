015b66bbfc09f24665153632e90869dd4fe8ad22
zakariya14 <zakariyaindonesia@gmail.com>
2021-11-22 20:41:37 +0700

    Update CRUD Comments

---
+++ app/Http/Controllers/API/CommentController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Comment::paginate(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'body' => 'required',
+            'meet_id' => 'required',
+            'reply_id' => 'required',
+            'created_by' => 'required'
+        ]);
+
+        $data = Comment::create($request->all());
+
+        return response()->json([
+            'message' => 'Tambah data berhasil',
+            'data' => $data
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comment $comment)
+    {
+        return response()->json([
+            'data' => $comment
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        $request->validate([
+            'body' => 'required',
+            'meet_id' => 'required',
+            'reply_id' => 'required',
+            'created_by' => 'required'
+        ]);
+
+        $comment->update($request->all());
+
+        return response()->json([
+            'message' => 'Ubah data berhasil',
+            'data' => $comment
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return response()->json([
+            'message' => 'Hapus data Berhasil'
+        ], 200);
+    }
+}
---
+++ database/migrations/2021_11_17_125102_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->timestamps();
+
+            $table->integer('meet_id');
+            $table->integer('reply_id');
+            $table->integer('created_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
